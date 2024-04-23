e99c8fa4a274e47ca4477eac71c469785fc331bf
Ahmed Abd El-latif <ahmedmahmoudfci@gmail.com>
2024-04-23 19:53:08 +0200

    ups

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -4,9 +4,15 @@ import { CommonModule } from '@angular/common';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { LayoutComponent } from './layout.component';
 import { HeaderComponent } from '../shared/header/header.component';
+import { FooterComponent } from '../shared/footer/footer.component';
 
 @NgModule({
   declarations: [LayoutComponent],
-  imports: [CommonModule, LayoutRoutingModule, HeaderComponent],
+  imports: [
+    CommonModule,
+    LayoutRoutingModule,
+    HeaderComponent,
+    FooterComponent,
+  ],
 })
 export class LayoutModule {}
