c3fd904886d81aad9b89e8e5ae0c840b4a2c2051
Ahmed Abd El-latif <ahmedmahmoudfci@gmail.com>
2024-04-23 11:20:58 +0200

    ups

--- src/app/layout/components/home/components/stats/stats.component.ts
+++ src/app/layout/components/home/components/stats/stats.component.ts
@@ -6,8 +6,29 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './stats.component.html',
-  styleUrls: ['./stats.component.scss']
+  styleUrls: ['./stats.component.scss'],
 })
 export class StatsComponent {
-
+  statsData = [
+    {
+      icon: 'far fa-user fa-2x fa-fw',
+      number: '300',
+      text: 'Clients',
+    },
+    {
+      icon: 'fas fa-code fa-2x fa-fw',
+      number: '400',
+      text: 'projects',
+    },
+    {
+      icon: 'fas fa-globe fa-2x fa-fw',
+      number: '12',
+      text: 'Countries',
+    },
+    {
+      icon: 'far fa-money-bill-alt fa-2x fa-fw',
+      number: '500k',
+      text: 'Money',
+    },
+  ];
 }
